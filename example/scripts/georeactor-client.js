69461c41e1bab8efdcf3d78f1eadc768112b5fe5
Nick Doiron <ndoiron@mapmeld.com>
2015-12-18 16:23:04 +0545

    React connects map selection and sidebar

--- example/scripts/georeactor-client.js
+++ example/scripts/georeactor-client.js
@@ -7,43 +7,65 @@ var initMap;
     map = new google.maps.Map(document.getElementById(georeactor.div), {
       zoom: 5,
       center: {lat: 0, lng: 0},
-      mapTypeId: google.maps.MapTypeId.TERRAIN
+      mapTypeId: google.maps.MapTypeId.TERRAIN,
+      streetViewControl: false
     });
 
-    // XMLHttpRequest without jQuery
-    var request = new XMLHttpRequest();
-    request.open('GET', 'data/townships.topojson', true);
+    var bounds = null;
+    function makeRequestFor(datafile) {
+      // XMLHttpRequest without jQuery
+      var df = datafile;
+      var request = new XMLHttpRequest();
+      request.open('GET', datafile, true);
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4) {
-        if (this.status >= 200 && this.status < 400) {
-          var tj = JSON.parse(this.responseText);
-          var key = Object.keys(tj.objects)[0];
-          var gj = topojson.feature(tj, tj.objects[key]);
-
-          var bounds = null;
-          for (var f = 0; f < gj.features.length; f++) {
-            bounds = makeBounds(gj.features[f].geometry.coordinates, bounds);
-          }
-          map.fitBounds(new google.maps.LatLngBounds(
-            new google.maps.LatLng(bounds[1], bounds[0]),
-            new google.maps.LatLng(bounds[3], bounds[2])
-          ));
-
-          map.data.addGeoJson(gj);
-          map.data.setStyle(function (feature) {
-            return {
-              fillColor: '#f00',
-              strokeColor: '#444',
-              strokeWeight: 1
+      request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+          if (this.status >= 200 && this.status < 400) {
+            // consume GeoJSON or TopoJSON file
+            var gj = null;
+            var datafile = df.toLowerCase();
+            if (datafile.indexOf('topojson') > -1 || datafile.indexOf('topo.json') > -1) {
+              var tj = JSON.parse(this.responseText);
+              var key = Object.keys(tj.objects)[0];
+              gj = topojson.feature(tj, tj.objects[key]);
+            } else if (datafile.indexOf('geojson') > -1 || datafile.indexOf('geo.json') > -1) {
+              gj = JSON.parse(this.responseText);
+            } else {
+              throw 'data type unknown: ' + datafile;
             }
-          });
-        } else {
-          console.log('failed to do XMLHttpRequest');
+
+            // combine bounds for each data file
+            for (var f = 0; f < gj.features.length; f++) {
+              bounds = makeBounds(gj.features[f].geometry.coordinates, bounds);
+            }
+            map.fitBounds(new google.maps.LatLngBounds(
+              new google.maps.LatLng(bounds[1], bounds[0]),
+              new google.maps.LatLng(bounds[3], bounds[2])
+            ));
+
+            map.data.addGeoJson(gj);
+            map.data.setStyle(function (feature) {
+              return {
+                fillColor: '#f00',
+                strokeColor: '#444',
+                strokeWeight: 1
+              }
+            });
+            map.data.addListener('click', function(event) {
+              detailView.setState({ selectFeature: event.feature });
+            });
+          } else {
+            console.log('failed to do XMLHttpRequest');
+          }
         }
-      }
-    };
-    request.send();
+      };
+      request.send();
+    }
+    for (var d = 0; d < georeactor.data.length; d++) {
+      makeRequestFor(georeactor.data[d]);
+    }
+
+    initReact();
   };
 
   function makeBounds(coordinates, existing) {
---
+++ example/scripts/georeactor-detail.js
@@ -0,0 +1,31 @@
+var detailView;
+
+function initReact() {
+  var MapDetail = React.createClass({
+    getInitialState: function() {
+      return { selectFeature: null };
+    },
+
+    render: function() {
+      console.log('checking');
+      if (!this.state.selectFeature) {
+        return (
+          <p>
+            Hello, <input type="text" placeholder="Your name here" />!
+          </p>
+        );
+      } else {
+        return (
+          <p>
+            Got it: {this.state.selectFeature.getProperty('Myanmar3')}
+          </p>
+        );
+      }
+    }
+  });
+
+  detailView = ReactDOM.render(
+    <MapDetail/>,
+    document.getElementById('sidebar')
+  );
+}
